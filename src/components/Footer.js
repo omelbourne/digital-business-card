27adcca258843e62afaa02e11cb2aab12c690861
omelbourne <43779654+omelbourne@users.noreply.github.com>
2022-02-22 00:07:52 +0000

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+      <footer className="Footer">
+        <div className = "SocialButtons">
+            <img />
+            <img />
+            <img />
+            <img />
+        </div>
+      </footer>
+    )
+  }
---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+export default function Main() {
+    return (
+      <main className="Main">
+        <h1>Oliver Melbourne</h1>
+        <p className = "Role">Frontend Developer</p>
+        <p className = "Website">oliver.melbourne</p>
+        
+        <div className = "MainButtons">
+            <button className = "Email">Email</button>
+            <button className = "LinkedIn">LinkedIn</button>
+        </div>
+        
+        <h3>About</h3>
+        <p>Example about me text consisting areas of expertise and aspirations.</p>
+
+        <h3>Interests</h3>
+        <p>Example interests text.</p>
+
+      </main>
+    )
+  }
